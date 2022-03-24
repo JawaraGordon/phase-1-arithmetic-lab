0df7ac1d9681745319f0ea85cd81cd986173ed89
JawaraGordon <jawaragordon@icloud.com>
2022-03-23 20:52:04 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+
+// const chai = 'chai'
+
+
+
+/* function multiply(num1,num2) {
+    return num1 * num2;
+}
+
+multiply(2,31); */
+
+
+/* const multiply = 2 * 31;
+console.log(multiply); */
+
+const num1 = 2
+const num2 = 31
+
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+const num3 = 10
+const num4 = 6
+
+const mod = num3 % num4; 
+
+const max = Math.max(16, 17, 18, 19, 20);
